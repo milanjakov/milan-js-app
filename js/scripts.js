c88e27650f38cd9482ba7a3da36d0ea89b88a218
milanjakov <mile_milisav@yahoo.com>
2025-04-07 23:07:53 -0400

    update changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+// Declaration of pokemon array with each object
 let pokemonList = [
-  { name: 'Bulbasaur', Height: 7, type:['Grass', 'Monster'] },
-  { name: 'Pikachu', Height: 4, type: ['Electric'] },
-  { name: 'Squirtle', Height: 5, type: ['Water'] }
+  { name: 'Bulbasaur',
+     Height: 7, 
+     type:['Grass', 'Monster'] },
+  { name: 'Pikachu', 
+     Height: 4, 
+    type: ['Electric'] },
+  { name: 'Squirtle',
+     Height: 5, 
+     type: ['Water'] }    
 ];
+
